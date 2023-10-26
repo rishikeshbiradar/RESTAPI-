87bd7528fc910fe41eac92288f1f99398671585b
Rushikesh Shivaji Biradar <115314561+rishikeshbiradar@users.noreply.github.com>
2023-10-27 03:44:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const PORT = process.env.PORT || 5000;
 const courses_router = require("./routes/courses")
 const connectDB = require("./db/connect")
 require("dotenv").config();
 
+app.use(express.static("Code"))
+app.use('/static', express.static("static"));
+
 app.get("/", (req, res) => {
-    res.send("Hello All");
+    res.sendFile(path.join(__dirname, "code/index.html"))
 })
 
 app.use("/api/courses", courses_router)
